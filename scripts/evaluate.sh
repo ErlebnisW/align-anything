7fd7356c53a39f329143a1eb7568fc7b8924f04e
ErlebnisW <787368748w@gmail.com>
2024-10-12 00:20:28 +0800

    add eval scripts

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -14,21 +14,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+# export CUDA_VISIBLE_DEVICES=0
+export HF_ENDPOINT="https://hf-mirror.com"
+
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${SCRIPT_DIR}/../align_anything/evaluation" || exit 1
 
-BENCHMARKS=("")
-OUTPUT_DIR=""
-GENERATION_BACKEND=""
-MODEL_ID=""
-MODEL_NAME_OR_PATH=""
-CHAT_TEMPLATE=""
+BENCHMARKS=("mt_bench")
+OUTPUT_DIR=/data/wmz_workspace/align-anything/eval_results
+GENERATION_BACKEND=deepspeed
+MODEL_ID=gemma_alpaca_sft_ds
+MODEL_NAME_OR_PATH=/data/wmz_workspace/checkpoints/gemma-2b_alpaca_sft
+CHAT_TEMPLATE=Gemma
 
 for BENCHMARK in "${BENCHMARKS[@]}"; do
     python __main__.py \
         --benchmark ${BENCHMARK} \
-        --output_dir "${OUTPUT_DIR}/${BENCHMARK}" \
+        --output_dir "${OUTPUT_DIR}/${BENCHMARK}/${MODEL_ID}" \
         --generation_backend ${GENERATION_BACKEND} \
         --model_id ${MODEL_ID} \
         --model_name_or_path ${MODEL_NAME_OR_PATH} \
